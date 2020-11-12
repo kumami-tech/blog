d4a1fbdc19d16ea492315ce096df9bb550b45b93
kumami-tech <sk924731@gmail.com>
2020-11-12 18:04:10 +0900

    [add]postsのバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :image, presence: true
 end
