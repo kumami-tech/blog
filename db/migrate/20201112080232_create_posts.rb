3f1b1909ab20710916fca3ce72ae245e1010fbba
kumami-tech <sk924731@gmail.com>
2020-11-12 18:00:17 +0900

    [fix]postsのマイグレーションファイルの修正

--- db/migrate/20201112080232_create_posts.rb
+++ db/migrate/20201112080232_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.string :title,               null: false
       t.string :content,             null: false
       t.string :image,               null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
