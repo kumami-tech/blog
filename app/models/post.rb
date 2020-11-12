82b1fe2a6647fd8d250455ae3c309a28d80e1f75
kumami-tech <sk924731@gmail.com>
2020-11-12 18:47:54 +0900

    [add]carrierwaveの導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,6 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :image, presence: true
+
+  mount_uploader :image, ImageUploader
 end
