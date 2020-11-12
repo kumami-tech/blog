c43edf716b110dcd136e202149baae09df13f7cb
kumami-tech <sk924731@gmail.com>
2020-11-12 18:49:06 +0900

    [add]新規投稿機能のサーバーサイドの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      flash[:notice] = '投稿しました。'
+      redirect_to root_path
+    else
+      flash[:alert] = '入力に不備があります。'
+      render "new"
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image).merge(user_id: current_user.id)
+  end
 end
