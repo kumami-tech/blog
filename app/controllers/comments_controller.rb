4e4f23904fa82473666b4a6fc89f0a842e08d3ef
kumami-tech <sk924731@gmail.com>
2020-11-17 14:42:27 +0900

    [update]commentコントローラーの不要なコードの削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
     respond_to do |format|
-      format.html { redirect_to tweet_path(params[:tweet_id])  }
       format.json
     end
   end
