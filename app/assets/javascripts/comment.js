6c0ae8e88307f665db0b7339000e07ad97e7774d
kumami-tech <sk924731@gmail.com>
2020-11-19 18:02:16 +0900

    [update]comment.jsの修正

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,6 +1,6 @@
-window.addEventListener("load", function() {
-  const token = document.getElementsByName("csrf-token")[0].content;
-  function createHTML(comment) {
+document.addEventListener('DOMContentLoaded', () => {
+  const token = document.getElementsByName('csrf-token')[0].content;
+  const createHTML = (comment) => {
     const strHtml = '<div class="Comment">' +
                       '<div class="Comment_info">' +
                         '<div class="Nickname">' + comment.nickname + '</div>' +
@@ -10,41 +10,44 @@ window.addEventListener("load", function() {
                         '<p>' + comment.content + '</p>' +
                       '</div>' +
                     '</div>';
-    const divElm = document.createElement("div");
+    const divElm = document.createElement('div');
     divElm.innerHTML = strHtml;
     return divElm.children[0];
   };
 
-  document.getElementById("Comment_form").addEventListener("submit", function(e) {
+  document.getElementById('Comment_form').addEventListener('submit', e => {
     e.preventDefault();
-    const inputNickname = document.getElementById("Nickname_field").value;
-    const inputContent = document.getElementById("Content_field").value;
-    const url = document.getElementById("Comment_form").getAttribute("action") + ".json";
+
+    const inputNickname = document.getElementById('Nickname_field').value;
+    const inputContent = document.getElementById('Content_field').value;
+    const url = document.getElementById('Comment_form').getAttribute('action') + '.json';
     const hashData = {
       comment: { nickname: inputNickname, content: inputContent }
     };
     const data = JSON.stringify(hashData);
     const xmlHR = new XMLHttpRequest();
-    xmlHR.open("POST", url, true);
-    xmlHR.responseType = "json";
-    xmlHR.setRequestHeader("Content-Type", "application/json");
-    xmlHR.setRequestHeader("X-CSRF-Token", token);
+    const FormBtn = document.getElementById('Comment_form__Btn');
+
+    xmlHR.open('POST', url);
+    xmlHR.responseType = 'json';
+    xmlHR.setRequestHeader('Content-Type', 'application/json');
+    xmlHR.setRequestHeader('X-CSRF-Token', token);
     xmlHR.send(data);
-    xmlHR.onreadystatechange = function() {
+    xmlHR.onreadystatechange = () => {
       if (xmlHR.readyState === 4) {
         if (xmlHR.status === 200) {
           const comment = xmlHR.response;
           const html = createHTML(comment);
-          const comments = document.getElementById("Comment_index");
+          const comments = document.getElementById('Comment_index');
           comments.insertBefore(html, comments.firstChild);
-          document.getElementById("Nickname_field").value = "";
-          document.getElementById("Content_field").value = "";
+          document.getElementById('Nickname_field').value = '';
+          document.getElementById('Content_field').value = '';
         } else {
-          alert("入力に不備があります");
+          alert('入力に不備があります');
         }
-        document.getElementById("Comment_form__Btn").disabled = false;
-        document.getElementById("Comment_form__Btn").removeAttribute("data-disable-with");
+        FormBtn.disabled = false;
+        FormBtn.removeAttribute('data-disable-with');
       }
     };
-  }, false);
+  });
 });
