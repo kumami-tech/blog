708a74b571bc61d498947afc24fa8becdafd6a1b
kumami-tech <sk924731@gmail.com>
2020-11-19 17:49:52 +0900

    [update]コメント機能の素のjs化

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,42 +1,50 @@
-$(function(){
-  function buildHTML(comment){
-    const html = `<div class="Comment">
-                  <div class="Comment_info">
-                    <div class="Nickname">
-                      ${comment.nickname}
-                    </div>
-                    <div class="Time">
-                      ${comment.created_at}
-                    </div>
-                  </div>
-                  <div class="Comment_content">
-                    <p>${comment.content}</p>
-                  </div>
-                </div>`
-    return html;
-  }
-  $('.Comment_form').on('submit', function(e){
+window.addEventListener("load", function() {
+  const token = document.getElementsByName("csrf-token")[0].content;
+  function createHTML(comment) {
+    const strHtml = '<div class="Comment">' +
+                      '<div class="Comment_info">' +
+                        '<div class="Nickname">' + comment.nickname + '</div>' +
+                        '<div class="Time">' + comment.created_at + '</div>' +
+                      '</div>' +
+                      '<div class="Comment_content">' +
+                        '<p>' + comment.content + '</p>' +
+                      '</div>' +
+                    '</div>';
+    const divElm = document.createElement("div");
+    divElm.innerHTML = strHtml;
+    return divElm.children[0];
+  };
+
+  document.getElementById("Comment_form").addEventListener("submit", function(e) {
     e.preventDefault();
-    const formData = new FormData(this);
-    const url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      const html = buildHTML(data);
-      $('.Comment_index').prepend(html);
-      $('.Comment_form__Field').val('');
-      $('.Comment_form__Btn').prop('disabled', false);
-      $('.Comment_empty').hide();
-    })
-    .fail(function(){
-      alert('入力に不備があります');
-      $('.Comment_form__Btn').prop('disabled', false);
-    })
-  })
+    const inputNickname = document.getElementById("Nickname_field").value;
+    const inputContent = document.getElementById("Content_field").value;
+    const url = document.getElementById("Comment_form").getAttribute("action") + ".json";
+    const hashData = {
+      comment: { nickname: inputNickname, content: inputContent }
+    };
+    const data = JSON.stringify(hashData);
+    const xmlHR = new XMLHttpRequest();
+    xmlHR.open("POST", url, true);
+    xmlHR.responseType = "json";
+    xmlHR.setRequestHeader("Content-Type", "application/json");
+    xmlHR.setRequestHeader("X-CSRF-Token", token);
+    xmlHR.send(data);
+    xmlHR.onreadystatechange = function() {
+      if (xmlHR.readyState === 4) {
+        if (xmlHR.status === 200) {
+          const comment = xmlHR.response;
+          const html = createHTML(comment);
+          const comments = document.getElementById("Comment_index");
+          comments.insertBefore(html, comments.firstChild);
+          document.getElementById("Nickname_field").value = "";
+          document.getElementById("Content_field").value = "";
+        } else {
+          alert("入力に不備があります");
+        }
+        document.getElementById("Comment_form__Btn").disabled = false;
+        document.getElementById("Comment_form__Btn").removeAttribute("data-disable-with");
+      }
+    };
+  }, false);
 });
