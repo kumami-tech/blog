b0bd8609eddea796c04743424a19bdc632abd8cb
kumami-tech <sk924731@gmail.com>
2020-11-23 12:08:58 +0900

    [update]バリデーションの修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   scope :sorted, -> { order(created_at: :desc) }
 
+  belongs_to :user
   belongs_to :post
 
-  validates :nickname, presence: true, length: { maximum: 20 }
   validates :content,  presence: true, length: { maximum: 1000 }
 end
